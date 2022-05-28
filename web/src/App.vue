9bc3cd0c91123a7e8fcb19448d8ea87f8ad49444
Administrator <1739951529@qq.com>
2022-05-28 14:03:36 +0800

    3.4 制作footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,26 +1,22 @@
 <template>
   <a-layout>
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-        theme="dark"
-        mode="horizontal"
-        v-model:selectedKeys="selectedKeys1"
-        :style="{ lineHeight: '64px' }"
-      >
-
-
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+    <the-header></the-header>
       <router-view/>
-    <a-layout-footer style="text-align: center">
-    传不习乎？知识分享库 Design ©2018 Created By  SSPU-LiaoLiao
-     </a-layout-footer>
+      <the-footer></the-footer>
   </a-layout>
 </template>
+<script lang="ts">
+import {defineComponent} from "vue";
+import TheHeader from '@/components/the-header.vue';
+import TheFooter from '@/components/the-footer.vue';
+export default defineComponent({
+  name: 'app',
+components:{
+    TheHeader,
+    TheFooter,
+},
+});
+</script>
 
 <style>
 #components-layout-demo-top-side-2 .logo {
---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    传不习乎？知识分享  库Design ©2018 Created By  SSPU-LiaoLiao
+  </a-layout-footer>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer',
+
+});
+</script>
