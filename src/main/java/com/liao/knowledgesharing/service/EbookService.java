20ab0c913d382b43ee6d7eae6a3b47447b851e5b
Administrator <1739951529@qq.com>
2022-05-28 19:53:04 +0800

    4.5VUE多环境，增加dev和prod 配置文件 编译和启动命令增加多环境

--- src/main/java/com/liao/knowledgesharing/service/EbookService.java
+++ src/main/java/com/liao/knowledgesharing/service/EbookService.java
@@ -9,6 +9,7 @@ import com.liao.knowledgesharing.resp.EbookResp;
 import com.liao.knowledgesharing.utils.CopyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import java.util.List;
 
@@ -21,9 +22,12 @@ public class EbookService {
     {
         EbookExample ebookExample=new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();//Criteria相当于where
-        criteria.andNameLike("%"+req.getName()+"%");
+        if(!ObjectUtils.isEmpty(req.getName())){
 
+            criteria.andNameLike("%"+req.getName()+"%");
+        }
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
 //        List<EbookResp> respList=new ArrayList<>();
 //        for (Ebook ebook : ebookList) {
 ////            EbookResp ebookResp = new EbookResp();
--- web/src/main.ts
+++ web/src/main.ts
@@ -5,4 +5,17 @@ import store from './store'
 //引入antd组件
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
-createApp(App).use(store).use(router).use(Antd).mount('#app');
+import * as Icons  from '@ant-design/icons-vue';//圖標
+const app=createApp(App);
+app.use(store).use(router).use(Antd).mount('#app');
+// 全局使用圖表
+const  icons: any =Icons;
+for (const i in Icons) {
+    app.component(i,icons[i]);
+
+}
+console.log('环境',process.env.NODE_ENV);
+console.log('客户端',process.env.VUE_APP_INTERFACE_URL);
+console.log('客户端',process.env.BASE_URL);
+console.log('客户端',process.env);
+
